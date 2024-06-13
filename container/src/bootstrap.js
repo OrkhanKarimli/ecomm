43b90ef9bb183a1e2047feb9e3de56488a0a89d2
OrkhanKarimli <orkhansk@code.edu.az>
2024-06-13 11:52:51 +0400

    Refactoring two elements

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,4 @@
-import 'products/ProductsIndex';
-import {mount} from 'cart/CartIndex';
-mount(document.querySelector('#my-cart'));
+import {mount as productMount} from 'products/ProductsIndex';
+import {mount as cartMount} from 'cart/CartIndex';
+productMount(document.querySelector('#my-cart'));
+cartMount(document.querySelector('.my-pro'));
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,8 +1,18 @@
 import faker from 'faker';
-let products='';
-for(let i=0; i<9; i++){
-let name=faker.commerce.productName(0);
-products+=`<div>${name}<div/>`;
+
+const mount=(el)=>{
+    let products='';
+    for(let i=0; i<9; i++){
+    let name=faker.commerce.productName(0);
+    products+=`<div>${name}<div/>`;
+    }
+    console.log(products);
+    el.innerHTML=products;
 }
-console.log(products);
-document.querySelector(".dev-pro").innerHTML=products;
+if (process.env.NODE_ENV==="development"){
+    const el=document.querySelector('.dev-pro');
+    if(el){
+        mount(el);
+    }
+}
+export {mount};
